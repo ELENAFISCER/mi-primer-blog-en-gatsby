efcce62828c27ffe4c813f41bfb98d201f74c51d
elenafiscer <tandem.fiscer@gmail.com>
2024-03-22 09:10:49 +0100

    Paso 3 utilizar Plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,27 +7,29 @@ import {
   navLinkItem,
   navLinkText
 } from './layout.module.css'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Layout = ({ pageTitle, children }) => {
- return (
-   <div className={container}>
-     <nav>
-       <ul className={navLinks}>
-         <li className={navLinkItem}><Link className={navLinkText} to="/">Home</Link></li>
-         <li className={navLinkItem}><Link className={navLinkText}to="/sobremi">Sobre Mí</Link></li>
-         <li className={navLinkItem}><Link className={navLinkText} to="/blog">Blog</Link></li>
-         <li className={navLinkItem}><a className={navLinkText} href="https://github.com/canodelacuadra">Github</a></li>
-       </ul>
-     </nav>
-     <main>
-       <h1 className={heading}>{pageTitle}</h1>
-       {children}
-       <footer>
+return (
+  <div className={container}>
+    <StaticImage src="../images/logo.png" width= {100}>
+    <nav>
+      <ul className={navLinks}>
+        <li className={navLinkItem}><Link className={navLinkText} to="/">Home</Link></li>
+        <li className={navLinkItem}><Link className={navLinkText}to="/sobremi">Sobre Mí</Link></li>
+        <li className={navLinkItem}><Link className={navLinkText} to="/blog">Blog</Link></li>
+        <li className={navLinkItem}><a className={navLinkText} href="https://github.com/canodelacuadra">Github</a></li>
+      </ul>
+    </nav>
+    <main>
+      <h1 className={heading}>{pageTitle}</h1>
+      {children}
+      <footer>
         <p className="footer"> &copy; canodelacuadra {new Date().getFullYear()}</p>
-       
+      
         </footer>
-     </main>
-   </div>
- )
+    </main>
+  </div>
+)
 }
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 // Step 2: Define your component
 const IndexPage = () => {
- return (
-   <Layout pageTitle='mi página de inicio'>
+return (
+  <Layout pageTitle='mi página de inicio'>
+    
     <p>Aranjuez mola y mañana no lloverá</p>
-
-   </Layout>
- )
+    <StaticImage src="https://c8.alamy.com/compes/m0kacb/palacio-real-provincia-de-aranjuez-madrid-espana-m0kacb.jpg" alt=""/>
+<StaticImage src=""/>
+  </Layout>
+  
+  
+)
 }
 // You'll learn about this in the next task, just copy it for now
 export const Head = () => <title>Home Page</title>
